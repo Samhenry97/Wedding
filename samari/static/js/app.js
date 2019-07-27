8e4aec44b758626b9c8a8a7c1ff4a7aac4b0307e
Sam Henry <smlhnry@gmail.com>
2019-07-27 19:19:34 -0400

    Bridal Party Design, Header

--- samari/static/js/app.js
+++ samari/static/js/app.js
@@ -1,4 +1,7 @@
-$(document).ready(function(){
+var sticky = null;
+
+$(document).ready(function() {
+  // Owl Carousel for Quotes
   $('.owl-carousel').owlCarousel({
     items: 1,
     loop: true,
@@ -7,17 +10,39 @@ $(document).ready(function(){
     autoplay: true
   });
 
+  // Smooth Scroll
   $('a[href*="#"]').on('click', function(event) {
     event.preventDefault();
 
     $('html, body').animate(
       { scrollTop: $($(this).attr('href')).offset().top },
-      500,
-      'linear'
+      1500,
+      'easeInOutQuint'
     );
+  });
+
+  // Ensure on-scroll animations are correct
+  AOS.refresh();
+
+  // Mobile Menu
+  $('#hamburger').click(function(event) {
+    $('#nav-items').addClass('slide-in');
+  });
+  $('#nav-items').click(function(event) {
+    $('#nav-items').removeClass('slide-in');
   });
 });
 
-$(window).on('load', function () {
-  AOS.refresh();
-});
+// Calculate the header's sticky position
+window.onload = window.onresize = function() {
+  sticky = $('#home')[0].getBoundingClientRect().bottom + window.scrollY;
+}
+
+// Determind if sticky header should stick
+window.onscroll = function() {
+  if(window.scrollY > sticky) {
+    $('header').addClass('sticky');
+  } else {
+    $('header').removeClass('sticky');
+  }
+}
