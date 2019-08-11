4900c69cc0bcb441dfb1c0bb3de23d62629c813f
Sam Henry <smlhnry@gmail.com>
2019-08-10 22:44:46 -0400

    Grammar, Coming Dropdown

--- samari/forms.py
+++ samari/forms.py
@@ -6,7 +6,7 @@ class RSVPForm(forms.ModelForm):
     email = forms.CharField(help_text='Your Email*')
     number = forms.IntegerField(min_value=0, max_value=20, help_text='Number of Guests*')
     message = forms.CharField(help_text='Message', widget=forms.Textarea, required=False)
-    attending = forms.ChoiceField(choices=(('Yes', 'I\'m Attending'), ('No', 'I\'m Not Attending')))
+    attending = forms.ChoiceField(choices=(('Both', 'Attending Ceremony and Reception'), ('Ceremony', 'Attending Ceremony Only'), ('None', 'Regretfully Decline')))
 
     class Meta:
         model = RSVP
